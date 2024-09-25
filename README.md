8f142ea4bfacb463e92e08fed4e5d0189b971147
Christian <christianda3@gmail.com>
2024-09-25 15:12:45 -0400

    Added hello world file in csharp

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is meant to store different directories where i learn how to use different programming languages like C# C++ ruby and many others.
---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+// this is a single line comment
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello World!");
+    }
+}
+/* and this is a multi-
+line
+comment */

5b850fa1a63bdc1d65b96f65710f34a56410a117
Christian <christianda3@gmail.com>
2024-11-21 18:04:19 -0400

    Added a file exploring conditions in C#

---
+++ CSHARP/condition.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            time = 10;
+            if (time < 18)
+            {
+                Console.WriteLine("Good Day!");
+            }
+            else
+            {
+                Console.WriteLine("Good evning!");
+            }
+        }
+    }
+}

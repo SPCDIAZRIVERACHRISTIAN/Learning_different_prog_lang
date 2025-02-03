f7f9beaca49cf6adbc57a11130f701bd426a6337
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2025-02-03 11:51:30 -0400

    Started to make a little social media page to learn php

---
+++ PHP/socialWeb/README.md
@@ -0,0 +1,1 @@
+This is a social web page made on php html and css to understand how to make a web page using PHP.
---
+++ PHP/socialWeb/login.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <title>
+            Mybook | Log in
+        </title>
+    </head>
+    <style>
+        body {
+            font-family: tahoma;
+            background-color: #e9ebee;
+            margin: 0;
+            padding: 0;
+        }
+        #bar {
+            height: 100px;
+            background-color: #3c5a99;
+            color: #d9dfeb;
+            font-size: 40px;
+            padding: 4px;
+        }
+        #signup_button {
+            background-color: #42b72a;
+            width: 70px;
+            text-align: center;
+            padding: 4px;
+            border-radius: 4px;
+            float: right;
+            font-size: 20px;
+        }
+        #login_card {
+            background-color: white;
+            width: 800px;
+            height: 300px;
+            margin: auto;
+            margin-top: 50px;
+            padding: 10px;
+            text-align: center;
+            padding-top: 50px;
+        }
+        #text {
+            height: 40px;
+            width: 300px;
+            border-radius: 4px;
+            border: solid 1px #ccc;
+            padding: 4px;
+            font-size: 14px;
+        }
+        #button {
+            width: 300px;
+            height: 40px;
+            border-radius: 4px;
+            font-weight: bold;
+            border: none;
+            background-color: #3c5a99;
+            color: #d9dfeb;
+        }
+    </style>
+    <body>
+        <div id="bar">
+            Mybook
+            <div id="signup_button">Sign up</div>
+        </div>
+        <div id="login_card">
+            <h2>Log in to Mybook</h2><br><br>
+            <input type="text" id="text" placeholder="User Email"><br><br>
+            <input type="password" id="text" placeholder="User's Password"><br><br>
+            <input type="submit" id="button" value="Login"><br>
+        </div>
+    </body>
+</html>
+

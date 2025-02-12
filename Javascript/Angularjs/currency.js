9e61b6c9d032bd756082a9c0138a708874fe85be
Christian <christianda3@gmail.com>
2025-02-12 14:54:47 -0400

    Add AngularJS examples for basic math operations, currency display, filtering, form validation, and task management

---
+++ Javascript/Angularjs/currency.js
@@ -0,0 +1,24 @@
+angular.module('app', []).controller("MainController", function() {
+    this.items = [
+        {
+            name: "m249",
+            price: "10000"
+        },
+        {
+            name: "m67",
+            price: "200.95"
+        },
+        {
+            name: "m240",
+            price: "15407.77"
+        },
+        {
+            name: "FLCk",
+            price: ""
+        },
+        {
+            name: "Kevlar",
+            price: "priceless"
+        }
+    ]
+})
---
+++ Javascript/Angularjs/filtering.js
@@ -0,0 +1,28 @@
+angular.module('app', []).controller("MainController", function() {
+    this.people = [
+        {
+            name: "Minga Parmesana",
+            phone: "787-123987"
+        },
+        {
+            name: "Skif Blaskovich",
+            phone: "787-678905"
+        },
+        {
+            name: "Anderson Price",
+            phone: "787-567894"
+        },
+        {
+            name: "Louisianna Fast",
+            phone: "787-567894"
+        },
+        {
+            name: "Cup O. Coffee",
+            phone: "787-678905"
+        },
+        {
+            name: "Pizza Hutter",
+            phone: "787-678905"
+        }
+    ];
+});
